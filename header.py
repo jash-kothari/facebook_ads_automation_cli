8e71dd03398794c0bccccde18ffce5d424a5c9a6
dj_coder <jash.kothari@Mirraw.com>
2016-09-03 19:40:31 +0530

    Final CLI code

--- header.py
+++ header.py
@@ -12,12 +12,10 @@ my_app_id = os.environ['ADS_FB_APP_ID']
 my_app_secret = os.environ['ADS_FB_APP_SECRET']
 my_access_token = os.environ['ADS_FB_APP_ACCESS_TOKEN'] # Your user access token
 page_id = os.environ['FB_PAGE_ID']
-database = os.environ['FB_APP_DATABASE']
-host = os.environ['FB_APP_DATBASE_HOST']
-port = os.environ['FB_APP_DATBASE_PORT']
-user = os.environ['FB_APP_DATABASE_USER']
-password = os.environ['FB_APP_DATABASE_PASSWORD']
+database_url = os.environ['FB_APP_DATABASE_URL']
 FacebookAdsApi.init(my_app_id, my_app_secret, my_access_token)
 me = AdUser(fbid='me')
-my_account = me.get_ad_accounts()[1]
+# print(me.get_ad_accounts())
+my_account = me.get_ad_accounts()[0]
+print my_account
 my_account=json.loads(str(my_account).replace('<AdAccount> ',''))
